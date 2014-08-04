afbb44fc31b239358e9855bb3664045e24b7ccc8
Cosima Ara <cosima.ara@gmail.com>
2014-08-04 13:14:11 -0700

    sudo as root for docker is no longer required

--- amplab/docker-scripts/build/tag_all.sh
+++ amplab/docker-scripts/build/tag_all.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [[ "$USER" != "root" ]]; then
-    echo "please run as: sudo $0"
-    exit 1
-fi
-
 image_list=("spark-master:0.9.0" "spark-worker:0.9.0" "spark-shell:0.9.0" "shark-master:0.8.0" "shark-worker:0.8.0" "shark-shell:0.8.0" )
 
 IMAGE_PREFIX="commonera/"
