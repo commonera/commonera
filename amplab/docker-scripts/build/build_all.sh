6a82524a4b0a4556c499be5cb38bba672593961b
Cosima Ara <cosima.ara@gmail.com>
2014-08-04 11:55:59 -0700

    No need to run as root

--- amplab/docker-scripts/build/build_all.sh
+++ amplab/docker-scripts/build/build_all.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [[ "$USER" != "root" ]]; then
-    echo "please run as: sudo $0"
-    exit 1
-fi
-
 CURDIR=$(pwd)
 BASEDIR=$(cd $(dirname $0); pwd)"/.."
 dir_list=( "dnsmasq" "apache-hadoop-hdfs" "spark-0.7.3" "shark-0.7.0" "spark-0.8.0" "spark-0.9.0" "shark-0.8.0" )
