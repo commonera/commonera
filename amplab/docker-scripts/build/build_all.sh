0a14b5f40ad5286382efbbb587bf94eef3ca9178
Cosima Ara <cosima.ara@gmail.com>
2014-08-04 13:13:34 -0700

    Download external binaries ahead of time outside of docker ADD command

--- amplab/docker-scripts/build/build_all.sh
+++ amplab/docker-scripts/build/build_all.sh
@@ -2,7 +2,26 @@
 
 CURDIR=$(pwd)
 BASEDIR=$(cd $(dirname $0); pwd)"/.."
-dir_list=( "dnsmasq" "apache-hadoop-hdfs" "spark-0.7.3" "shark-0.7.0" "spark-0.8.0" "spark-0.9.0" "shark-0.8.0" )
+#dir_list=( "dnsmasq" "apache-hadoop-hdfs" "spark-0.7.3" "shark-0.7.0" "spark-0.8.0" "spark-0.9.0" "shark-0.8.0" )
+dir_list=( "dnsmasq" "apache-hadoop-hdfs" "spark-1.0.0")
+
+(cd apache-hadoop-hdfs/;
+if [ ! -e hadoop_1.2.1-1_x86_64.deb ]; then
+    wget http://mirror.sdunix.com/apache/hadoop/common/hadoop-1.2.1/hadoop_1.2.1-1_x86_64.deb
+fi
+)
+
+(cd spark-1.0.0/spark-base/ ;
+if [ ! -e scala-2.10.3.tgz ]; then
+    wget http://www.scala-lang.org/files/archive/scala-2.10.3.tgz
+fi
+)
+
+(cd spark-1.0.0/spark-base/ ;
+if [ ! -e spark-1.0.0-bin-hadoop1.tgz ]; then
+    wget http://d3kbcqa49mib13.cloudfront.net/spark-1.0.0-bin-hadoop1.tgz
+fi
+)
 
 export IMAGE_PREFIX="commonera/"
 #"commonera/"
